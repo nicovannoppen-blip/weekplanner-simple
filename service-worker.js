86ebfafd4dd3bcc6a4c219d4298b8dd767b752f7
nicovannoppen-blip <nicovannoppen@gmail.com>
2026-03-06 12:11:03 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,9 @@
-const CACHE = "agenda-cache-v1";
+self.addEventListener("install",e=>{
 
-const FILES = [
-"/",
-"/index.html",
-"/style.css",
-"/app.js",
-"/manifest.json"
-];
+self.skipWaiting();
 
-self.addEventListener("install", e=>{
-e.waitUntil(
-caches.open(CACHE)
-.then(cache=>cache.addAll(FILES))
-);
 });
 
-self.addEventListener("fetch", e=>{
-e.respondWith(
-caches.match(e.request)
-.then(r=>r || fetch(e.request))
-);
+self.addEventListener("fetch",e=>{
+
 });
