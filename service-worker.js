3cf623b7cf8072993e90dc6c70fd769cd67ba942
nicovannoppen-blip <nicovannoppen@gmail.com>
2026-03-06 22:32:08 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,15 @@
-self.addEventListener("install",event=>{
-self.skipWaiting()
-})
-
-self.addEventListener("fetch",event=>{})
+{
+"name": "AgendaPicto",
+"short_name": "Agenda",
+"start_url": "index.html",
+"display": "standalone",
+"background_color": "#ffffff",
+"theme_color": "#4CAF50",
+"icons": [
+{
+"src":"icons/school.png",
+"sizes":"192x192",
+"type":"image/png"
+}
+]
+}
