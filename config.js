bb27a029821703390d2be53b08430f5f26b15c7a
nicovannoppen-blip <nicovannoppen@gmail.com>
2026-03-10 02:48:25 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const CLIENT_ID="259423355709-81s2fclv800ps73gqm8vb7t4vkcvct81.apps.googleusercontent.com"
+
+const HIDDEN_CALENDARS=[
+"Weeknummers",
+"Agenda",
+"Borgloon Weather",
+"agenda website"
+]
