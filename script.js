c4fc44ad765351a3b650f9b41a01d6728be4eec9
nicovannoppen-blip <nicovannoppen@gmail.com>
2026-06-15 04:40:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ vissen: ["vissen","aquarium"],
 vuurtoren: ["vuurtoren"],
 Hunebed: ["hunebed"],
 bunker: ["bunker","bunkers"],
-ster: ["ster"],
+ster: ["ster","sterren"],
 orca: ["orca","orka"]  
 }
 
